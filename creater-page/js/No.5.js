77d6a3920facd3633977952356e348f78ce06b97
Nelu1223 <108245377+Nelu1223@users.noreply.github.com>
2026-09-20 22:19:40 +0900

    Add files via upload

---
+++ creater-page/js/No.5.js
@@ -0,0 +1,65 @@
+const galleryItems = document.querySelectorAll('.gallery-img-list-icon-img');
+const modalImg = document.getElementById('gallery-img');
+const modalTitle = document.getElementById('gallery-title');
+
+const modalCon = document.getElementById('gallery-comment');
+const modalCon1 = document.getElementById('gallery-description-con-1');
+const modalCon2 = document.getElementById('gallery-description-con-2');
+const modalCon3 = document.getElementById('gallery-description-con-3');
+const modalCon4 = document.getElementById('gallery-description-con-4');
+const modalCon5 = document.getElementById('gallery-description-con-5');
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+galleryItems.forEach(item => {
+  item.addEventListener('click', async () => {
+    console.log('clicked!');
+        //クリックされた画像に対応する全体画像を取得
+        const fullImgUrl = item.getAttribute('data-full') || item.src;
+        //モーダルの画像タグに全体画像を設定
+        modalImg.src = fullImgUrl;
+        //クリックされた画像に対応するタイトルを取得
+        const titleCon = item.getAttribute('alt') || "N/A";
+        //モーダルのタイトルタグにタイトルを設定
+        modalTitle.textContent = titleCon;
+        switch(titleCon){
+            case "結(ゆい)":
+                modalCon.textContent = "\"……\"";
+                modalCon1.textContent = "デコポニの激マブお姉さん";
+                modalCon2.textContent = "enfj、CV:東山奈央";
+                modalCon3.textContent = "";
+                modalCon4.textContent = "";
+                modalCon5.textContent = "";
+                break;
+            case "王 余暉(わん よき)":
+                modalCon.textContent = "\"……\"";
+                modalCon1.textContent = "お友達にはなりたくない人作ろ〜で生まれた子";
+                modalCon2.textContent = "中華マフィアで現在行方不明";
+                modalCon3.textContent = "りおんとはお友達";
+                modalCon4.textContent = "";
+                modalCon5.textContent = "";
+                break;
+            case "月の女神":
+                modalCon.textContent = "\"……\"";
+                modalCon1.textContent = "月の女神、圧倒的上位存在";
+                modalCon2.textContent = "逆らう者には容赦しない";
+                modalCon3.textContent = "上位存在であるため、人間が名前を知ることは許されていない";
+                modalCon4.textContent = "";
+                modalCon5.textContent = "";
+                break;
+            default:
+                modalCon.textContent = "";
+                modalCon1.textContent = "";
+                modalCon2.textContent = "";
+                modalCon3.textContent = "";
+                modalCon4.textContent = "";
+                modalCon5.textContent = "";
+        }
+  });
+});
+
+// モーダルボタンをクリックしたときのイベントを登録
+/*galleryItems.addEventListener('click', (e) => {
+        
+});
+*/
